a53e605f3d565a7a57180cfe50a3796bea43a3f1
LucasEscribe <lfd.meneses@gmail.com>
2023-07-03 22:37:41 -0300

    links API y spreadsheets para el prouecto (en App.tsx).

--- carrito2023/src/app/index.tsx
+++ carrito2023/src/app/index.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ul>
+          <li><a href="https://fakeapi.platzi.com/">Api</a></li>
+          <li><a href="https://fakeapi.platzi.com/en/gql/users/">Api: Users</a></li>
+          <li><a href="https://api.escuelajs.co/docs#">Docs JS</a></li>
+          <li><a href="https://docs.google.com/spreadsheets/d/1O5r02Vyt_DmjeymLF4QV_839i_0NQXee5XKCxx5bGRk/edit#gid=0">DriveInfoReact</a></li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
+  
+  
 }
 
 export default App
