084e9a1a3f89c81ab08d5a36e5aa9142fc4ab81a
LucasEscribe <lfd.meneses@gmail.com>
2023-07-29 22:56:07 -0300

    Estilos en Detalle del carrito.

--- src/app/pages/Cart/CartDetail/index.tsx
+++ src/app/pages/Cart/CartDetail/index.tsx
@@ -2,12 +2,24 @@ import React from "react";
 import { useCart } from "../../../contexts/CartContext";
 import ProductCard from "../../../components/ProductCard";
 import styles from "./styles.module.css";
+import Home from "../../../pages/Home";
 
 function CartDetail() {
   const { cartItems } = useCart();
 
+  if (cartItems.length === 0) {
+    return (
+      <div className={styles.gridContainer}>
+        <h1>Detalle de su carrito:</h1>
+        <p>Su carrito está vacío. ¡Agregue algunos productos!</p>
+        <Home />
+      </div>
+    );
+  }
+
   return (
     <div className={styles.gridContainer}>
+      <h1>Detalle de su carrito:</h1>
       {cartItems.map((product) => (
         <ProductCard
           key={product.id}
